7be7db9fc8ab4e826d9d7d45af8e4b51d9d9cdf1
Dinesh <dinesh@rocketsource.co>
2025-04-08 18:03:02 +0530

    Added admin request

--- app/routes/api/bundles.jsx
+++ app/routes/api/bundles.jsx
@@ -1,6 +1,8 @@
 import { json } from "@remix-run/node";
+import { authenticate } from "../../shopify.server";
 
 export const loader = async ({ request }) => {
+  await authenticate.admin(request);
   const url = new URL(request.url);
   const productId = url.searchParams.get("productId");
 
